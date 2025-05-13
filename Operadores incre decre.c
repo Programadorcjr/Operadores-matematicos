ef7830a0b6216ce1485bcd709240f6fbbe245bbc
Programadorjr <208865809+Programadorcjr@users.noreply.github.com>
2025-05-13 14:03:48 +0000

    commit incre decre

---
+++ Operadores incre decre.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    Incremento (++)
+    pré-incremento ++a
+    pós-incremento a++
+    Decremento (--)
+    pré-decremento --a
+    pós-decremento  a--
+    */
+
+    int numero1 = 1;
+
+    printf("Antes incremento: %d\n",numero1);
+    numero1++;
+    printf("Apos incremento: %d\n",numero1);
+     numero1--;
+    printf("Apos decremento: %d\n",numero1);
+
+
+
+
+}
+
